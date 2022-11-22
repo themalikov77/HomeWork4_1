cfb95d3b260912419a4ad1bbf2e2852a1090425f
Malikov Nurjigit <110462907+themalikov77@users.noreply.github.com>
2022-11-22 05:04:29 +0300

    save image and age in SharedPreferences

--- app/src/main/java/com/example/taskmanager/data/local/Pref.kt
+++ app/src/main/java/com/example/taskmanager/data/local/Pref.kt
@@ -3,6 +3,7 @@ package com.example.taskmanager.data.local
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.media.Image
 
 class Pref(private val context: Context) {
     private val pref: SharedPreferences = context.getSharedPreferences("pref_name", MODE_PRIVATE)
@@ -14,7 +15,30 @@ class Pref(private val context: Context) {
     fun saveShowBoarding(isShow:Boolean){
         pref.edit().putBoolean(BOARDING_SHOW,isShow).apply()
     }
+    fun savedText(name:String){
+         pref.edit().putString( NAME_PROFILE ,name).apply()
+    }
+    fun getName(): String? {
+        return pref.getString(NAME_PROFILE,"")
+    }
+    fun saveAge(age:String){
+       pref.edit().putString(AGE_PROFILE,age).apply()
+    }
+    fun getAge(): String? {
+        return pref.getString(AGE_PROFILE,"")
+    }
+    fun saveImage(image: String){
+        pref.edit().putString(KEY_IMAGE_URL,image).apply()
+    }
+    fun getImage(): String? {
+        return pref.getString(KEY_IMAGE_URL,"")
+
+    }
+
     companion object{
         const val BOARDING_SHOW ="onBoardingShow"
+        const val NAME_PROFILE="onName"
+        const val  AGE_PROFILE= "onAge"
+        const val KEY_IMAGE_URL = "Key_image_url"
     }
 }
--- app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
@@ -1,60 +1,60 @@
 package com.example.taskmanager.ui.profile
 
-import android.app.Activity
-import android.app.Activity.RESULT_OK
-import android.app.Notification.Action
-import android.content.Intent
-import android.graphics.Bitmap
+
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.taskmanager.R
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.taskmanager.data.local.Pref
 import com.example.taskmanager.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
-    private lateinit var selectedImg:Uri
-    companion object{
-        val IMAGE_REQUEST_CODE=1
+    private lateinit var pref: Pref
+    private val getContent: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri: Uri? ->
+            Glide.with(this).load(imageUri).into(binding.profileImage)
+            pref.saveImage(imageUri.toString())
+        }
+
+    companion object {
+        val TYPE = "image/*"
+
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentProfileBinding.inflate(inflater,container,false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.profileImage.setOnClickListener {
-            onClick()
+            getContent.launch(TYPE)
+
         }
-    }
+        pref = Pref(requireContext())
+        binding.outlinedEditText.setText(pref.getName())
+        binding.etAge.setText(pref.getAge())
+        Glide.with(this).load(pref.getImage()).into(binding.profileImage)
+        binding.outlinedEditText.addTextChangedListener {
+            pref.savedText(binding.outlinedEditText.text.toString())
+            pref.saveAge(binding.etAge.text.toString())
 
-    private fun onClick() {
-        val intent = Intent()
-        intent.action=Intent.ACTION_GET_CONTENT
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (data != null) {
-            if (data.data != null){
-                selectedImg=data.data!!
-                binding.profileImage.setImageURI(selectedImg)
-
-
-            }
         }
+
+
     }
-
-
-
 }
+
+
